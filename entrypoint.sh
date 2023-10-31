bc11a18612682a9c6d0aa821a877c11515671fb8
patrick.xu <xusenqi@gmail.com>
2023-10-31 21:18:46 +0800

    entrypoint.sh Adapt to the Linux environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
+# dependencies: (1) celestia-app v1.1.0 
+#               (2) celestia v0.11.0
 
-CHAINID="test"
+set -x
+
+CHAINID="private"
+MONIKER="validator1"
+KEY_NAME=validator
 
 # App & node has a celestia user with home dir /home/celestia
-APP_PATH="/home/celestia/.celestia-app"
-NODE_PATH="/home/celestia/bridge/"
+if grep -q "init" /proc/1/cgroup; then
+    echo "Running on Linux Ubuntu host"
+    ENV="linux"
+    APP_PATH="/data/celestia/.celestia-app"
+    NODE_PATH="/data/celestia/bridge/"
+else
+    echo "Running in Docker container"
+    ENV="docker"
+    APP_PATH="/home/celestia/.celestia-app"
+    NODE_PATH="/home/celestia/bridge/"
+fi
+
 
 # Check if the folder exists
 if [ -d "$APP_PATH" ]; then
@@ -17,26 +33,41 @@ else
   echo "The folder $APP_PATH does not exist."
 fi
 
+# Check if the folder exists
+if [ -d "$NODE_PATH" ]; then
+  # If it exists, delete it
+  echo "The folder $NODE_PATH exists. Deleting it..."
+  rm -rf "$NODE_PATH"
+  echo "Folder deleted."
+else
+  # If it doesn't exist, print a message
+  echo "The folder $NODE_PATH does not exist."
+fi
+
 # Build genesis file incl account for passed address
 coins="1000000000000000utia"
-celestia-appd init $CHAINID --chain-id $CHAINID
-celestia-appd keys add validator --keyring-backend="test"
+celestia-appd init $MONIKER --chain-id $CHAINID --home $APP_PATH
+celestia-appd keys add $KEY_NAME --keyring-backend="test" --home $APP_PATH
 # this won't work because some proto types are declared twice and the logs output to stdout (dependency hell involving iavl)
-celestia-appd add-genesis-account $(celestia-appd keys show validator -a --keyring-backend="test") $coins
-celestia-appd gentx validator 5000000000utia \
+celestia-appd add-genesis-account $KEY_NAME $coins --home $APP_PATH
+celestia-appd gentx $KEY_NAME 5000000000utia \
+  --home $APP_PATH \
   --keyring-backend="test" \
   --chain-id $CHAINID
 
-celestia-appd collect-gentxs
+celestia-appd collect-gentxs --home $APP_PATH  
 
 # Set proper defaults and change ports
 # If you encounter: `sed: -I or -i may not be used with stdin` on MacOS you can mitigate by installing gnu-sed
 # https://gist.github.com/andre3k1/e3a1a7133fded5de5a9ee99c87c6fa0d?permalink_comment_id=3082272#gistcomment-3082272
-sed -i'.bak' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/timeout_commit = "25s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
+sed -i'.bak' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $APP_PATH/config/config.toml
+#sed -i 's/grpc_laddr = ""/grpc_laddr = "tcp:\/\/0.0.0.0:9090"/' $APP_PATH/config/config.toml
+
+#等待把 grpc_laddr = "" 设置成 "tcp://0.0.0.0:9090"
+#sed -i'.bak' 's/timeout_commit = "25s"/timeout_commit = "1s"/g' $APP_PATH/config/config.toml
+#sed -i'.bak' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $APP_PATH/config/config.toml
+#sed -i'.bak' 's/index_all_keys = false/index_all_keys = true/g' $APP_PATH/config/config.toml
+#sed -i'.bak' 's/mode = "full"/mode = "validator"/g' $APP_PATH/config/config.toml
 
 # Register the validator EVM address
 {
@@ -57,7 +88,11 @@ mkdir -p $NODE_PATH/keys
 cp -r $APP_PATH/keyring-test/ $NODE_PATH/keys/keyring-test/
 
 # Start the celestia-app
-celestia-appd start &
+if [ "$ENV" = "linux" ]; then
+    nohup celestia-appd start --home $APP_PATH &> celestia-appd.log &
+else
+    celestia-appd start --home $APP_PATH &
+fi
 
 # Try to get the genesis hash. Usually first request returns an empty string (port is not open, curl fails), later attempts
 # returns "null" if block was not yet produced.
@@ -70,13 +105,21 @@ while [ "${#GENESIS}" -le 4 -a $CNT -ne $MAX ]; do
 	sleep 1
 done
 
-export CELESTIA_CUSTOM=test:$GENESIS
+export CELESTIA_CUSTOM=private:$GENESIS
 echo $CELESTIA_CUSTOM
 
-celestia bridge init --node.store /home/celestia/bridge
+celestia bridge init --node.store $NODE_PATH
 export CELESTIA_NODE_AUTH_TOKEN=$(celestia bridge auth admin --node.store ${NODE_PATH})
 echo "WARNING: Keep this auth token secret **DO NOT** log this auth token outside of development. CELESTIA_NODE_AUTH_TOKEN=$CELESTIA_NODE_AUTH_TOKEN"
-celestia bridge start \
-  --node.store $NODE_PATH --gateway \
-  --core.ip 127.0.0.1 \
-  --keyring.accname validator
+
+if [ "$ENV" = "linux" ]; then
+    nohup celestia bridge start \
+      --node.store $NODE_PATH --gateway \
+      --core.ip 127.0.0.1 \
+      --keyring.accname validator &> bridge.log &
+else
+    celestia bridge start \
+      --node.store $NODE_PATH --gateway \
+      --core.ip 127.0.0.1 \
+      --keyring.accname validator
+fi
